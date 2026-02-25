3797d94791ef67aa52c1869362836451971198ba
Soomin <soomin02094@gmail.com>
2026-02-25 20:06:06 +0900

    3-5 Spring Boot + Redis 연동 실습 - LocalTime & LocalDateTime 파싱 설정

--- src/main/java/org/example/plus/common/config/RedisConfig.java
+++ src/main/java/org/example/plus/common/config/RedisConfig.java
@@ -1,5 +1,10 @@
 package org.example.plus.common.config;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -12,21 +17,36 @@ public class RedisConfig {
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
+
+        // 추가 Java 8에서 추가된 LocalDate, LocalDateTime을 커버하기 위한 설정
+        // ObjectMapper 커스터마이징
+        ObjectMapper mapper = new ObjectMapper()
+                .registerModule(new JavaTimeModule())
+                .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+                .activateDefaultTyping(
+                        LaissezFaireSubTypeValidator.instance,
+                        ObjectMapper.DefaultTyping.NON_FINAL,
+                        JsonTypeInfo.As.PROPERTY
+                );
+
+        GenericJackson2JsonRedisSerializer serializer =
+                new GenericJackson2JsonRedisSerializer(mapper);
 
         // Redis에서는 어떤 타입으로 보내던 문자열로 저장
         // 자바에서 특정 타입으로 전달 -> Redis에 문자열로 저장
         // Redis에서 문자열로 받아옴 -> 자바에서 타입 불일치 발생
         // 따라서 JSON 파싱해주는 설정 필요
+
         template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        template.setValueSerializer(serializer);
 
         template.setHashKeySerializer(new StringRedisSerializer());
-        template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+        template.setHashValueSerializer(serializer);
 
         template.afterPropertiesSet();
         return template;
     }
-
 }
--- src/main/java/org/example/plus/domain/post/model/dto/PostDto.java
+++ src/main/java/org/example/plus/domain/post/model/dto/PostDto.java
@@ -15,8 +15,10 @@ public class PostDto {
     private long id;
     private String content;
     private String username;
+    private LocalDate check;
+    // Redis 시간을 나타내는 LocalDate, LocalDateTime을 사용하기 위해 설정 추가 -> RedisConfig
 
     public static PostDto from(Post post, String username) {
-        return new PostDto(post.getId(), post.getContent(), username);
+        return new PostDto(post.getId(), post.getContent(), username, LocalDate.now());
     }
 }
